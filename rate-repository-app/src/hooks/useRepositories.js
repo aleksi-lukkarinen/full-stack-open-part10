303191c6d7adb60cb944c10d445fdd09747c0c10
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-09-04 23:04:14 +0300

    Ex. 10.11 done

---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,27 @@
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+
+
+const useRepositories = () => {
+  const { data, error, loading } =
+    useQuery(GET_REPOSITORIES, {
+      fetchPolicy: "cache-and-network",
+    });
+
+  if (loading || error) {
+    return {
+      repositories: null,
+      loading,
+      error,
+    };
+  }
+
+  return {
+    repositories: data.repositories,
+    loading,
+    error,
+  };
+};
+
+export default useRepositories;
