34861f0bf8cf31c71de56a5b2c58da8d562335a7
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-09-09 12:54:58 +0300

    Sets up React Native Paper

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { AppRegistry } from "react-native";
 import { NativeRouter } from "react-router-native";
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import { ApolloProvider } from "@apollo/client";
 
+import Constants from 'expo-constants';
 import AuthStorage from "./src/utils/authStorage";
 import createApolloClient from "./src/utils/apolloClient";
 import Main from "./src/components/Main";
@@ -18,11 +21,16 @@ const App = () => {
     <NativeRouter>
       <ApolloProvider client={apolloClient}>
         <AuthStorageContext.Provider value={authStorage}>
-          <Main />
+          <PaperProvider>
+            <Main />
+          </PaperProvider>
         </AuthStorageContext.Provider>
       </ApolloProvider>
     </NativeRouter>
   );
 };
+
+const appName = Constants.manifest.name;
+AppRegistry.registerComponent(appName, App);
 
 export default App;
